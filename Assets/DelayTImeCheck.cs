a8eb7c22f0ac769ec4178419d3988669f1a96a09
ManikandanSRE <ManikandanSRE@users.noreply.github.com>
2018-10-01 16:35:27 +0530

    Worked on Sample Scene
    Tested one special festure
    
    Co-Authored-By: ragavendran <ragavendran@users.noreply.github.com>

---
+++ Assets/DelayTImeCheck.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DelayTImeCheck : MonoBehaviour {
+    public Text Text;
+    public int time;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Text.text=Time.unscaledDeltaTime.ToString();
+	}
+    public void StopGame()
+    {
+        StartCoroutine(waittime(time));
+       
+
+    }
+
+    private IEnumerator waittime(int value)
+    {
+        Time.timeScale = 0;
+       
+        yield return new WaitForSecondsRealtime(3f);
+        Time.timeScale = 1;
+    }
+}
